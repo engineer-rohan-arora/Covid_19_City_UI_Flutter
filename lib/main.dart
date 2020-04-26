6ee6222abbeb98369368efcd97c7ec23c80e14cd
Rohan Arora <engineer.rohan.arora@gmail.com>
2020-04-27 00:24:10 +0530

    updated info_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'widget/my_header.dart';
 import 'package:saharanpurcovidapp/constant.dart';
 import 'widget/counter.dart';
@@ -118,45 +117,19 @@ class _HomeScreen extends State<HomeScreen> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(
-                      "Symptoms",
-                      style: kTitleTextstyle,
-                    ),
                     SizedBox(height: 20),
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          SymptomCard(
-                            image: "assets/images/headache.png",
-                            title: "Headache",
-                            isActive: true,
-                          ),
-                          SymptomCard(
-                            image: "assets/images/caugh.png",
-                            title: "Caugh",
-                          ),
-                          SymptomCard(
-                            image: "assets/images/fever.png",
-                            title: "Fever",
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text("Prevention", style: kTitleTextstyle),
+                    Center(child: Text("Announcements", style: kTitleTextstyle)),
                     SizedBox(height: 20),
                     PreventCard(
                       text:
                       "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks",
-                      image: "assets/images/wear_mask.png",
+
                       title: "Wear face mask",
                     ),
                     PreventCard(
                       text:
                       "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks",
-                      image: "assets/images/wash_hands.png",
+
                       title: "Wash your hands",
                     ),
                     SizedBox(height: 50),
@@ -172,12 +145,10 @@ class _HomeScreen extends State<HomeScreen> {
 
 }
 class PreventCard extends StatelessWidget {
-  final String image;
   final String title;
   final String text;
   const PreventCard({
     Key key,
-    this.image,
     this.title,
     this.text,
   }) : super(key: key);
@@ -187,12 +158,12 @@ class PreventCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
       child: SizedBox(
-        height: 156,
+        height: 160,
         child: Stack(
           alignment: Alignment.centerLeft,
           children: <Widget>[
             Container(
-              height: 136,
+              height: 140,
               width: double.infinity,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
@@ -206,37 +177,35 @@ class PreventCard extends StatelessWidget {
                 ],
               ),
             ),
-            Image.asset(image),
             Positioned(
-              left: 130,
+
               child: Container(
                 padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                height: 136,
-                width: MediaQuery.of(context).size.width - 170,
+                height: 140,
+
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Text(
-                      title,
-                      style: kTitleTextstyle.copyWith(
-                        fontSize: 16,
-                      ),
-                    ),
-                    Expanded(
+                    Center(
                       child: Text(
-                        text,
-                        maxLines: 4,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 12,
+                        title,
+                        style: kTitleTextstyle.copyWith(
+                          fontSize: 16,
                         ),
                       ),
                     ),
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: SvgPicture.asset("assets/icons/forward.svg"),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          text,
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
                     ),
+
                   ],
                 ),
               ),
@@ -248,47 +217,3 @@ class PreventCard extends StatelessWidget {
   }
 }
 
-class SymptomCard extends StatelessWidget {
-  final String image;
-  final String title;
-  final bool isActive;
-  const SymptomCard({
-    Key key,
-    this.image,
-    this.title,
-    this.isActive = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        color: Colors.white,
-        boxShadow: [
-          isActive
-              ? BoxShadow(
-            offset: Offset(0, 10),
-            blurRadius: 20,
-            color: kActiveShadowColor,
-          )
-              : BoxShadow(
-            offset: Offset(0, 3),
-            blurRadius: 6,
-            color: kShadowColor,
-          ),
-        ],
-      ),
-      child: Column(
-        children: <Widget>[
-          Image.asset(image, height: 90),
-          Text(
-            title,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-    );
-  }
-}
